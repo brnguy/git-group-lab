9436a60e8f07ef722f8d12183c3c5ea4c3d334cd
zbrose <zrbrose@gmail.com>
2022-03-23 16:20:07 -0700

    added about zach

---
+++ src/AboutZach.jsx
@@ -0,0 +1,11 @@
+function AboutZach() {
+    return ( 
+        <>
+         <h1>Zach Brose</h1>
+         <p>This is my description</p>
+         <img src='aboutzach.jpeg' alt='image' width="300px" />
+        </>
+     );
+}
+
+export default AboutZach
