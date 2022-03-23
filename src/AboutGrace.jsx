7e7719834ad8e36a6f90ed76b86960602d3362d7
kokopuffz <chanheeya@gmail.com>
2022-03-23 18:40:02 -0500

    aboutgrace

---
+++ src/AboutGrace.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function AboutGrace() {
+  return (
+    <>
+      <p> hi im grace</p>
+      <img
+        src="https://cdn.discordapp.com/attachments/919468128432455700/956329784126898246/powerpuffz-profile_image-0571b1f37d136f22-70x70.jpg"
+        alt=""
+      />
+    </>
+  )
+}
