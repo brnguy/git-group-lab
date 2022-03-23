d6e0ac5f50eff26edcc9e725f4ab32c6c2b45c33
tristonpalacios <triston.j.palacios@gmail.com>
2022-03-23 16:19:11 -0700

    added aboutTriston

---
+++ src/AboutTriston.jsx
@@ -0,0 +1,13 @@
+export default function AboutTriston(){
+    return(
+        <>
+        
+        <h1>Hello I am Tom Bombadil</h1>
+        <img src="https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/09/tom-bombadil.jpg" style={{width:250 , height:250}} />
+        <h3>  Hey dol! merry dol! ring a dong dillo!
+Ring a dong! hop along! Fal lal the willow!
+Tom Bom, jolly Tom, Tom Bombadillo!  </h3>
+
+        </>
+    )
+}
