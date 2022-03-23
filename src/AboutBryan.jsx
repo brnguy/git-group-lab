076f82892976171e6be72301322b58e5ef8bedbf
brnguy <nguyen.bryan15@gmail.com>
2022-03-23 16:30:44 -0700

    Added AboutBryan

---
+++ src/AboutBryan.jsx
@@ -0,0 +1,12 @@
+
+function AboutBryan() {
+    return (  
+        <>
+            <img src='https://i.imgur.com/MGtnYzD.jpg' alt="Bryan"/>
+            <h1>Bryan</h1>
+            <p>I like to party</p>
+        </>
+    );
+}
+
+export default AboutBryan;
